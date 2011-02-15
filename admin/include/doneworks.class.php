f1c6ba2daa6ce317703cb6d9bd9723b698ba8527
GansukhB <bganaa2009@gmail.com>
2011-02-16 00:07:17 +0800

    doneworks class-д засвар орсон

--- admin/include/doneworks.class.php
+++ admin/include/doneworks.class.php
@@ -3,6 +3,8 @@
 	
 	class DoneWorks {
 		var $conn;
+		var $db_error_msg; 
+		
 		var $lang;
 		var $en_title;
 		var $mn_title;
@@ -19,18 +21,35 @@
 			$this->conn = new DB_Class();
 		}
 		
-		function Register(){
-			
+		function Insert($type_id, $mn_title, $en_title, $mn_desc, $en_desc, $image, $file, $active){
+			$query = "INSERT INTO done_works (type_id, mn_title, en_title, mn_desc, en_desc, image, file, active) 
+			VALUES($type_id, $mn_title, $en_title, $mn_desc, $en_desc, $image, $file, $active)";
+			$result = mysql_query($query);
+			if(!$result)
+				$this->db_error_msg .= mysql_error();
 		}
 		
+		
 		function get_done_work($id){
-			$query = "";
+			$query = "SELECT * FROM done_works WHERE id = ".$id;
+			$result = mysql_query($query);
+			$result = mysql_fetch_row($result);
+			
+			$this->type_id = $result['type_id'];
+			$this->en_title = $result['en_title'];
+			$this->mn_title = $result['mn_title'];
+			$this->en_desc = $result['en_desc'];
+			$this->mn_desc = $result['mn_desc'];
+			$this->image = $result['image'];
+			$this->file = $result['file'];
+			$this->active = $result['active'];
+			$this->created_at = $result['created_at'];
+			$this->updated_at = $result['updated_at'];
 		}
 		
 		function get_done_works(){
-			$query = "SELECT * R";
+			$query = "SELECT * FROM done_works";
 		}
-		
 	}
 	
 	class Audio extends DoneWorks{
@@ -40,11 +59,15 @@
 			$this->allowed_extensions = array(".mp3");
 		}
 	}
-	 class Video extends DoneWorks{
+	class Video extends DoneWorks{
 		var $allowed_extensions;
 		
 		function __construct(){
 			$this->allowed_extensions = array(".flv", ".mp4");
+		}
+		function getVideo(){
+			$vid = new Video();
+			//$rv = $vid->
 		}
 	} 
 ?>
